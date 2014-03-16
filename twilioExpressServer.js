78fbc179f5900364672234a0693b4c29ae57382b
lukeg101 <lukegeeson@googlemail.com>
2014-03-16 01:23:31 +0000

    yes

--- twilioExpressServer.js
+++ twilioExpressServer.js
@@ -1,11 +1,12 @@
-var express = require('express');
-
-var app = express();
-app.use(express.urlencoded());
-
-app.post('/inbound', function(request, response) {
-    response.type('text/xml');
-    response.send('<Response><Say>Hello there! Thanks for calling.</Say></Response>');
+// Your accountSid and authToken from twilio.com/user/account
+var accountSid = 'AC3be41d17da098e2a805c590ccb2c5e00';
+var authToken = '0075916a20e97ec65cb2f76c00f6fd5b';
+var client = require('twilio')(accountSid, authToken);
+  
+client.sms.messages.create({
+    body: "lol",
+    to: "+447597576473",
+    from: "+441424400057"
+}, function(err, message) {
+    process.stdout.write(message.sid);
 });
-
-app.listen(8080);

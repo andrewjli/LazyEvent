37e1d19e640cfb1cd4b81b84b17d82404189f1a0
lukeg101 <lukegeeson@googlemail.com>
2014-03-16 01:48:13 +0000

    t

--- twilioExpressServer.js
+++ twilioExpressServer.js
@@ -2,10 +2,14 @@
 var accountSid = 'AC3be41d17da098e2a805c590ccb2c5e00';
 var authToken = '0075916a20e97ec65cb2f76c00f6fd5b';
 var client = require('twilio')(accountSid, authToken);
-  
+
+num = "+447597576473"
+
+client.getSms(
+
 client.sms.messages.create({
     body: "lol",
-    to: "+447597576473",
+    to: num,
     from: "+441424400057"
 }, function(err, message) {
     process.stdout.write(message.sid);
